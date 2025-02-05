bf77b05273be3eb6f4cddb67334d795d963fa9ac
santiago <nunezdugar@gmail.com>
2025-02-05 18:14:12 -0300

    proyecto finalizado

--- router.js
+++ router.js
@@ -24,19 +24,29 @@ router.get('/',(req,res)=> {
 
       })
 
-   // RUTA PARA EDITAR REGISTROS 
+    // RUTA PARA ELIMINAR REGISTROS
+      router.get('/delete/:id',(req,res)=>{
+         const id = req.params.id;
+         conexion.query('DELETE FROM users WHERE id=?',[id],(error,results)=>{
+            if(error){
+               throw error;
+            }else {
+               res.redirect('/');
+                }
+         })
+       });
+
+    // RUTA PARA EDITAR REGISTROS 
      router.get('/edit/:id',(req,res)=>{
       const id = req.params.id;
-      conexion.query('SELECT * FROM users WHERE id=?',[id],(error,results)=>{
-         if(error){
-            throw error;
+         conexion.query('SELECT * FROM users WHERE id=?',[id],(error,results)=>{
+            if(error){
+               throw error;
 
-         }else {
-            res.render('edit',{user:results[0]});
-         }
-
-         
-      })
+            }else {
+               res.render('edit',{user:results[0]});
+                }
+      } )
 
      } )
 
@@ -47,7 +57,7 @@ router.get('/',(req,res)=> {
 
 const crud = require ('./controler/crud');
 router.post('/save',crud.save)
-
+router.post('/update',crud.update);
 
 
 module.exports = router;
